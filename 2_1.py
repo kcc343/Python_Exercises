3eef4d347be5912370026328ae5c541bc4a1dc06
Kelly Chhor <kcc343@gmail.com>
2018-12-19 11:53:01 -0800

    First commit

---
+++ 2_1.py
@@ -0,0 +1,12 @@
+print(5 ** 2)  # 25
+print(9 * 5)  # 45
+print(15 / 12)  # 1.25
+print(12 / 15)  # 0.8
+print(15 // 12)  # 1
+print(12 // 15)  # 0
+print(5 % 2)  # 1
+print(9 % 5)  # 4
+print(15 % 12)  # 3
+print(12 % 15)  # 12
+print(6 % 6)  # 0
+print(0 % 7)  # 0
